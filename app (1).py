d6de33af468c419a68d8050dda8474c21c36d2e0
prem chand <premchand2803@gmail.com>
2026-04-09 16:01:07 +0530

    first commit

---
+++ app (1).py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Azure DevOps Pipeline 🚀"
+
+@app.route("/health")
+def health():
+    return {"status": "UP"}, 200
+
+if __name__ == "__main__":
+    # IMPORTANT: host + port required
+    app.run(host="0.0.0.0", port=5000, debug=False)
